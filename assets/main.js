4ca38b900e73e953629b5ca603e80f9c9537fab8
ATirado <azuru5039@gmail.com>
2023-10-09 12:32:22 -0400

    added html css and js files

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+// const playerDeckEl =
+// const startBtn = the html start button;
+
+// playerDeck = [all card array]
+// computerDeck = [all the cards]
+
+// startBtn.addEventListener('click,', function()){
+//     the game to start (startgame)
+//     the start button to disappear
+// }
+
+// function startGame()
+//     shuffle both decks
+
+
+// function shuffle(deck)){
+//    huffles deck and sets it to computer or player
+//    return shuffled deck
+// }
